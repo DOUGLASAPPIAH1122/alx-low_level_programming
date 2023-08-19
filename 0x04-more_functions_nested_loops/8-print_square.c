495fc290cde8f74b25055ed99271a48c626a5a8a
DOUGLASAPPIAH1122 <douglasappiah11@gmail.com>
2023-08-19 04:16:03 -0700

    You are so much sunshine in eevry square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - function point a square
+ * @size: size of square
+ * Description: can only use_putchar to print. use '1' to print square.
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	y = 0;
+
+	if (size < 1)
+		_putchar('\n');
+
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#'');
+			x++
+		}
+		_putchar('\n');
+		y++;
+	}
+}
