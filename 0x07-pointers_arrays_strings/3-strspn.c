e90ace671e4cd486c6bdec851700b7d3ccdd2175
DOUGLASAPPIAH1122 <douglasappiah11@gmail.com>
2023-08-28 19:05:28 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -18,7 +18,7 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (index = 0; accept[index]; index++)
 		{
-			if (*s == accepts[index])
+			if (*s == accept[index])
 			{
 				bytes++;
 				break;
